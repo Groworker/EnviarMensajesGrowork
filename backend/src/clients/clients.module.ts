206ee5c26b0d35d985737c262ae9ee355aaacb03
GroWork <agenciasuiza8@gmail.com>
2026-02-04 13:17:19 +0000

    fix: Restore missing ClientsController and register it in module to fix 404s

--- backend/src/clients/clients.module.ts
+++ backend/src/clients/clients.module.ts
@@ -3,6 +3,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Client } from '../entities/client.entity';
 import { DeletionLog } from './entities/deletion-log.entity';
 import { ClientDeletionService } from './client-deletion.service';
+import { ClientsController } from './clients.controller';
 import { NotificationsModule } from '../notifications/notifications.module';
 
 @Module({
@@ -10,6 +11,7 @@ import { NotificationsModule } from '../notifications/notifications.module';
         TypeOrmModule.forFeature([Client, DeletionLog]),
         NotificationsModule,
     ],
+    controllers: [ClientsController],
     providers: [ClientDeletionService],
     exports: [ClientDeletionService],
 })
